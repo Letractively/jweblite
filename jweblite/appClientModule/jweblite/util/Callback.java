cb9bfa15417af9952a528f3ba8df94d873e26fb0
h920526 <h920526@9c642374-f376-7a8d-3735-f717f3596241>
2011-04-23 04:50:25 +0000

    new callback class

---
+++ jweblite/appClientModule/jweblite/util/Callback.java
@@ -0,0 +1,16 @@
+package jweblite.util;
+
+public class Callback<T> {
+
+	/**
+	 * Default constructor.
+	 */
+	public Callback() {
+		super();
+	}
+
+	public T callback() {
+		return null;
+	}
+
+}
