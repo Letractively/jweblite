748043445419cab73e9d535ccdadc6abef09ba84
h920526 <h920526@9c642374-f376-7a8d-3735-f717f3596241>
2011-05-21 14:17:42 +0000

    More EL function support

---
+++ jweblite/appClientModule/jweblite/util/el/CollectionELUtils.java
@@ -0,0 +1,10 @@
+package jweblite.util.el;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class CollectionELUtils {
+
+	private Log log = LogFactory.getLog(this.getClass());
+
+}
