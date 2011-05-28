59cd4d1f691e0123b590842ad4bf763e83649f15
h920526 <h920526@9c642374-f376-7a8d-3735-f717f3596241>
2011-05-28 12:37:55 +0000

    add RequestELUtils

--- jweblite/appClientModule/jweblite/util/el/RequestELUtils.java
+++ jweblite/appClientModule/jweblite/util/el/RequestELUtils.java
@@ -3,32 +3,14 @@ package jweblite.util.el;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public class NumberELUtils {
+public class RequestELUtils {
 
 	private Log log = LogFactory.getLog(this.getClass());
 
 	/**
 	 * ========================================================================
-	 * java.lang.Number
+	 * jweblite.web.JWebLiteRequestWrapper
 	 * ========================================================================
 	 */
-
-	/**
-	 * Parse Int
-	 * 
-	 * @param str
-	 *            String
-	 * @return int
-	 */
-	public static int parseInt(String str) {
-		if (str == null) {
-			return 0;
-		}
-		try {
-			return Integer.parseInt(str);
-		} catch (Exception e) {
-		}
-		return 0;
-	}
 
 }
