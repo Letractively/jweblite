a801e6ad21f0459cf8336428b2c1e32735395423
h920526 <h920526@9c642374-f376-7a8d-3735-f717f3596241>
2011-06-06 11:27:00 +0000

    version 0.2.2 released : Add PagingTag

---
+++ trunk/jweblite/appClientModule/jweblite/web/tag/paging/FirstTag.java
@@ -0,0 +1,66 @@
+package jweblite.web.tag.paging;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.tagext.Tag;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import jweblite.data.provider.DataProvider;
+
+public class FirstTag extends TagSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	private Boolean test = null;
+
+	/**
+	 * Default constructor.
+	 */
+	public FirstTag() {
+		super();
+	}
+
+	@Override
+	public int doStartTag() throws JspException {
+		Tag tag = this.getParent();
+		if (tag == null || !(tag instanceof PagingTag)) {
+			throw new JspTagException("parent tag error");
+		}
+		PagingTag parent = (PagingTag) tag;
+		DataProvider provider = parent.getProvider();
+		int firstIndex = 0;
+		// test
+		if (this.test == null) {
+			if (provider == null || provider.getCurrentIndex() - 2 < firstIndex) {
+				return TagSupport.SKIP_BODY;
+			}
+		} else if (!this.test.booleanValue()) {
+			return TagSupport.SKIP_BODY;
+		}
+		// index
+		if (parent.getIndex() != null) {
+			this.pageContext.setAttribute(parent.getIndex(), firstIndex);
+		}
+		return TagSupport.EVAL_BODY_INCLUDE;
+	}
+
+	/**
+	 * Get Test
+	 * 
+	 * @return Boolean
+	 */
+	public Boolean getTest() {
+		return test;
+	}
+
+	/**
+	 * Set Test
+	 * 
+	 * @param test
+	 *            Boolean
+	 */
+	public void setTest(Boolean test) {
+		this.test = test;
+	}
+
+}
---
+++ trunk/jweblite/appClientModule/jweblite/web/tag/paging/PagingTag.java
@@ -0,0 +1,136 @@
+package jweblite.web.tag.paging;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import jweblite.data.provider.DataProvider;
+
+public class PagingTag extends TagSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	private DataProvider provider = null;
+
+	private String index = null;
+	private String var = null;
+	private String total = null;
+
+	/**
+	 * Default constructor.
+	 */
+	public PagingTag() {
+		super();
+	}
+
+	@Override
+	public int doStartTag() throws JspException {
+		// init
+		if (this.provider == null) {
+			return TagSupport.SKIP_BODY;
+		}
+		// var
+		if (this.var != null) {
+			this.pageContext.setAttribute(this.var, this.provider);
+		}
+		// total
+		if (this.total != null) {
+			this.pageContext.setAttribute(this.total,
+					this.provider.getTotalPageCount());
+		}
+		return TagSupport.EVAL_BODY_INCLUDE;
+	}
+
+	@Override
+	public int doEndTag() throws JspException {
+		// index
+		if (this.index != null) {
+			this.pageContext.removeAttribute(this.index);
+		}
+		// var
+		if (this.var != null) {
+			this.pageContext.removeAttribute(this.var);
+		}
+		// total
+		if (this.total != null) {
+			this.pageContext.removeAttribute(this.total);
+		}
+		return TagSupport.EVAL_PAGE;
+	}
+
+	/**
+	 * Get Provider
+	 * 
+	 * @return DataProvider
+	 */
+	public DataProvider getProvider() {
+		return provider;
+	}
+
+	/**
+	 * Set Provider
+	 * 
+	 * @param provider
+	 *            DataProvider
+	 */
+	public void setProvider(DataProvider provider) {
+		this.provider = provider;
+	}
+
+	/**
+	 * Get Index
+	 * 
+	 * @return String
+	 */
+	public String getIndex() {
+		return index;
+	}
+
+	/**
+	 * Set Index
+	 * 
+	 * @param index
+	 *            String
+	 */
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	/**
+	 * Get Var
+	 * 
+	 * @return String
+	 */
+	public String getVar() {
+		return var;
+	}
+
+	/**
+	 * Set Var
+	 * 
+	 * @param var
+	 *            String
+	 */
+	public void setVar(String var) {
+		this.var = var;
+	}
+
+	/**
+	 * Get Total
+	 * 
+	 * @return String
+	 */
+	public String getTotal() {
+		return total;
+	}
+
+	/**
+	 * Set Total
+	 * 
+	 * @param var
+	 *            String
+	 */
+	public void setTotal(String total) {
+		this.total = total;
+	}
+
+}
