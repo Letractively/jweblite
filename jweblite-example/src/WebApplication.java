2b315686bda1443ce4a2495e7b7755728e74b53f
h920526 <h920526@9c642374-f376-7a8d-3735-f717f3596241>
2011-06-18 07:21:53 +0000

    add HelloApplication

---
+++ jweblite-example/src/WebApplication.java
@@ -0,0 +1,20 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class WebApplication {
+
+	private static final long serialVersionUID = 1L;
+
+	private String startedAt = null;
+
+	public WebApplication() {
+		super();
+		this.startedAt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
+				.format(new Date());
+	}
+
+	public String getStartedAt() {
+		return startedAt;
+	}
+
+}
