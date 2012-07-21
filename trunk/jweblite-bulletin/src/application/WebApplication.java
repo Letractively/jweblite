064432d75e893444d6b5765bb5e34d69455e65e3
h920526@gmail.com <h920526@gmail.com@9c642374-f376-7a8d-3735-f717f3596241>
2012-07-21 08:13:56 +0000

    init

---
+++ trunk/jweblite-bulletin/src/application/WebApplication.java
@@ -0,0 +1,14 @@
+package application;
+
+import jweblite.web.application.JWebLiteApplication;
+
+public class WebApplication extends JWebLiteApplication {
+
+	/**
+	 * Default constructor.
+	 */
+	public WebApplication() {
+		super();
+	}
+
+}
