c92fa8a9ab7fea50a81674d52049981946757aa4
h920526 <h920526@gmail.com>
2012-12-16 05:09:59 +0000

    change to junit3

--- jWebLite/test/jweblite/web/dispatcher/JWebLiteRequestDispatcherTest.java
+++ jWebLite/test/jweblite/web/dispatcher/JWebLiteRequestDispatcherTest.java
@@ -1,83 +1,72 @@
 package jweblite.web.dispatcher;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import junit.swingui.TestRunner;
 
-public class JWebLiteRequestDispatcherTest {
+public class JWebLiteRequestDispatcherTest extends TestCase {
 
 	private JWebLiteRequestDispatcher dispatcher = null;
 
-	@Before
+	public static void main(String[] args) {
+		TestRunner.run(JWebLiteRequestDispatcherTest.class);
+	}
+
 	public void setUp() throws Exception {
 		this.dispatcher = new JWebLiteRequestDispatcher();
 	}
 
-	@After
 	public void tearDown() throws Exception {
 	}
 
-	@Test
 	public void testHomePath() {
 		Assert.assertNull(this.dispatcher.dispatch("/"));
 	}
 
 	// ------------------------- illegal ------------------------------
 
-	@Test
 	public void testIllegal1() {
 		Assert.assertNull(this.dispatcher.dispatch(null));
 	}
 
-	@Test
 	public void testIllegal2() {
 		Assert.assertNull(this.dispatcher.dispatch(""));
 	}
 
-	@Test
 	public void testIllegal3() {
 		Assert.assertNull(this.dispatcher.dispatch("."));
 	}
 
-	@Test
 	public void testIllegal4() {
 		Assert.assertNull(this.dispatcher.dispatch("/Test."));
 	}
 
-	@Test
 	public void testIllegal5() {
 		Assert.assertNull(this.dispatcher.dispatch("/Te.st."));
 	}
 
-	@Test
 	public void testIllegal6() {
 		Assert.assertNull(this.dispatcher.dispatch("/Te..st"));
 	}
 
-	@Test
 	public void testIllegal7() {
 		Assert.assertNull(this.dispatcher.dispatch("/./Test"));
 	}
 
 	// ------------------------- legal ------------------------------
 
-	@Test
 	public void testLegal1() {
 		Assert.assertEquals("test", this.dispatcher.dispatch("/test.jsp"));
 	}
 
-	@Test
 	public void testLegal2() {
 		Assert.assertEquals("tEsT", this.dispatcher.dispatch("/tEsT.jsp"));
 	}
 
-	@Test
 	public void testLegal3() {
 		Assert.assertEquals(".Test", this.dispatcher.dispatch("//Test.jsp"));
 	}
 
-	@Test
 	public void testLegal4() {
 		Assert.assertEquals("test.Test",
 				this.dispatcher.dispatch("/test/Test.jsp"));
